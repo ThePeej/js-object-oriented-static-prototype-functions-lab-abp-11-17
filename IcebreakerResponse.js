9d84cdda7d1d901d05982e3836a9261002cd7952
PJ Frias <peejfrias@gmail.com>
2017-11-10 21:24:30 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,8 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(emailArray) {
+  return emailArray.map(function(email) {
+    return new IcebreakerResponse(email)
+  })
+}
